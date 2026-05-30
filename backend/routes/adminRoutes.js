514abf34aa632163a9e241b86c557771e082b177
sachin7con <sachin7con@gmail.com>
2026-05-30 09:02:58 +0530

    fix admin

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const jwt = require("jsonwebtokens")
+const jwt = require("jsonwebtoken")
 
 const router = express.Router();
 
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,9 @@
-import Navigate from "react-router-dom";
+import {Navigate} from "react-router-dom";
 
 export default function ProtectedRoute({children}){
 
   const token = localStorage.getItem("token");
-  
+
     if(!token){
         return <Navigate to="/login" />;
     }
