7016643d52e3f33da1c65ad29a6005bc6831f2d7
sachin7con <sachin7con@gmail.com>
2026-06-07 00:55:36 +0530

    updated search and status

--- backend/models/Inquiry.js
+++ backend/models/Inquiry.js
@@ -4,6 +4,11 @@ const inquirySchema = mongoose.Schema({
     name: String,
     studentClass: String,
     phone: String,
+
+    status: {
+        type: String,
+        default: "New",
+    },
 }, {timestamps: true})
 
 module.exports = mongoose.model("Inquiry", inquirySchema);
--- backend/routes/inquiryRoutes.js
+++ backend/routes/inquiryRoutes.js
@@ -1,44 +1,66 @@
-const express = require("express");
-const Inquiry = require("../models/Inquiry");
-const router = express.Router();
+    const express = require("express");
+    const Inquiry = require("../models/Inquiry");
+    const router = express.Router();
 
 
-router.post("/", async (req, res) =>{
+    router.post("/", async (req, res) =>{
+        try{
+            const inquiry = new Inquiry(req.body)
+            await inquiry.save();
+
+            res.status(201).json({success: true, message: "Inquiry Submitted", })
+
+
+        }catch(error){
+            res.status(500).json({ success: false, message: error.message, })
+
+        }
+    } );
+
+    router.get("/", async (req, res) =>{
+        try{
+            const inquiries = await Inquiry.find().sort({ createdAt: -1});
+
+            res.json(inquiries);
+        } catch (error) {
+            res.status(500).json({message: error.message, })
+        }
+    })
+
+    //Update
+    router.put("/:id", async (req, res) =>{
     try{
-        const inquiry = new Inquiry(req.body)
-        await inquiry.save();
 
-        res.status(201).json({success: true, message: "Inquiry Submitted", })
+        console.log("ID:", req.params.id);
+        console.log("STATUS:", req.body.status);
 
+        const inquiry = await Inquiry.findByIdAndUpdate(
+            req.params.id,
+            { status: req.body.status },
+            { new: true }
+        );
 
-    }catch(error){
-        res.status(500).json({ success: false, message: error.message, })
+        console.log("UPDATED:", inquiry);
 
-    }
-} );
+        res.json(inquiry);
 
-router.get("/", async (req, res) =>{
-    try{
-        const inquiries = await Inquiry.find().sort({ createdAt: -1});
-
-        res.json(inquiries);
     } catch (error) {
-        res.status(500).json({message: error.message, })
-    }
-})
+        console.log(error);
+        res.status(500).json(error);
+    } });
 
-//delete inquiry route
-router.delete("/:id",async (req, res) =>{
-    try{
-        await Inquiry.findOneAndDelete(req.params.id)
+    //delete inquiry route
+    router.delete("/:id",async (req, res) =>{
+        try{
+            await Inquiry.findByIdAndDelete(req.params.id)
 
-        res.json({success: true, message: "inquiry deleted", })
-    }
-    catch(error){
-        res.status(500).json({
-            message: error.message,
-        });
-    }
-});  
+            res.json({success: true, message: "inquiry deleted", })
+        }
+        catch(error){
+            res.status(500).json({
+                message: error.message,
+            });
+        }
+    });  
 
-module.exports = router;
+    module.exports = router;
